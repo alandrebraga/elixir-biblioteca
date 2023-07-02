c02ec40ce7f244e4382e7004ece18e1cc0027e1c
Andre Luiz Mallmann <andre.mallmann@gmail.com>
2023-07-01 21:16:48 -0300

    feat: added route for create new books and authors

--- lib/biblioteca_web/controllers/biblioteca_controller.ex
+++ lib/biblioteca_web/controllers/biblioteca_controller.ex
@@ -1,7 +1,11 @@
 defmodule BibliotecaWeb.BibliotecaController do
   use BibliotecaWeb, :controller
 
-  def(home(conn, _params)) do
+  def home(conn, _params) do
     render(conn, :home, layout: false)
+  end
+
+  def index(conn, _params) do
+    render(conn, :index, layout: false)
   end
 end
--- lib/biblioteca_web/router.ex
+++ lib/biblioteca_web/router.ex
@@ -18,6 +18,7 @@ defmodule BibliotecaWeb.Router do
     pipe_through :browser
 
     get "/", BibliotecaController, :home
+    resources "/cadastro", BibliotecaController, only: [:index]
   end
 
   # Other scopes may use custom stacks.
